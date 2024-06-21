c5eec32b0ec20208252c5153e11e3cfa6f0dbc4b
luisbrandino <luisbrandino.contato@gmail.com>
2024-06-21 16:34:27 -0300

    build: added abstract Person model

---
+++ UPBank.Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace UPBank.Models
+{
+    public abstract class Person
+    {
+        public string Cpf {  get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public char Gender { get; set; }
+        public double Salary { get; set; }
+        public string Phone {  get; set; }
+        public string Email { get; set; }
+        public Address Address { get; set; }
+
+        [JsonIgnore]
+        public string AddressZipcode { get; set; }
+
+        /* 
+         * Como vocês vão ter que pegar endereço de outro banco de dados, é preciso que 
+         * tenha a chave do endereço aqui no Person para que vcs consigam pegar o endereço
+         * na api e depois colocar o resposta da api com o endereço no atributo Address.
+         * 
+         * A annotation [JsonIgnore] é pra que quando vcs forem listar no controller ele 
+         * não mostre esse campo, pois não é necessário
+         */
+    }
+}
