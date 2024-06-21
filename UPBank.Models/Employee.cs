f74dea2ffd06c4dca6f58dfb678aca9398c97cf9
luisbrandino <luisbrandino.contato@gmail.com>
2024-06-21 16:51:43 -0300

    build: new attribute to Employee model

--- UPBank.Models/Employee.cs
+++ UPBank.Models/Employee.cs
@@ -1,8 +1,21 @@
-﻿namespace UPBank.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UPBank.Models
 {
     public class Employee : Person
     {
         public int Register;
         public bool Manager { get; set; }
+
+        [NotMapped]
+        public Agency Agency { get; set; }
+
+        /*
+         * A annotation serve para que, ao criar a migration dessa entidade, o .Net não tente criar
+         * uma tabela Agency no banco de dados, já que essa informação pertence à outra database, em uma outra API.
+         * Através da API de Agency será possivel resgatar a Agency desse funcionário e popular o atributo Agency.
+         * 
+         * Se não forem usar Entity Framework, podem ignorar esse [NotMapped]
+         */
     }
 }
