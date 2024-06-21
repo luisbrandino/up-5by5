fdb0e9b397c3546d30ad0ada219784724678f003
luisbrandino <luisbrandino.contato@gmail.com>
2024-06-21 17:11:22 -0300

    build: added Transaction model

---
+++ UPBank.Models/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using UPBank.Enums;
+
+namespace UPBank.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public DateTime EffectiveDate { get; set; }
+        public EType Type { get; set; }
+        public double Value;
+
+        [NotMapped]
+        public Account? Destiny { get; set; }
+
+        [JsonIgnore]
+        public string? DestinyNumber {  get; set; }
+    }
+}
