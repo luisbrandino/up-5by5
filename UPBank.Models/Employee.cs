4b0eb03fbcfafee1b35d87af0505b7604c5b2389
luisbrandino <luisbrandino.contato@gmail.com>
2024-06-21 16:54:14 -0300

    build: change to Employee Model's documentation

--- UPBank.Models/Employee.cs
+++ UPBank.Models/Employee.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace UPBank.Models
 {
@@ -10,12 +11,16 @@ namespace UPBank.Models
         [NotMapped]
         public Agency Agency { get; set; }
 
+        [JsonIgnore]
+        public string AgencyNumber { get; set; } 
+
         /*
          * A annotation serve para que, ao criar a migration dessa entidade, o .Net não tente criar
          * uma tabela Agency no banco de dados, já que essa informação pertence à outra database, em uma outra API.
-         * Através da API de Agency será possivel resgatar a Agency desse funcionário e popular o atributo Agency.
+         * Através da API de Agency será possivel resgatar a Agency desse funcionário através desse atributo AgencyNumber (que será salvo no banco de dados de Employee) 
+         * e popular o atributo Agency.
          * 
-         * Se não forem usar Entity Framework, podem ignorar esse [NotMapped]
+         * Se não forem usar Entity Framework, podem ignorar esse [NotMapped] e [JsonIgnore]
          */
     }
 }
