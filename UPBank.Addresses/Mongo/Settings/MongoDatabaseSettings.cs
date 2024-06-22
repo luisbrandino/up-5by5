c5a1830446d95d05c3c3cbfc4559675b1564104d
luisbrandino <luisbrandino.contato@gmail.com>
2024-06-22 13:25:36 -0300

    build: added mongo connection string

--- UPBank.Addresses/Mongo/Settings/MongoDatabaseSettings.cs
+++ UPBank.Addresses/Mongo/Settings/MongoDatabaseSettings.cs
@@ -2,7 +2,7 @@
 {
     public class MongoDatabaseSettings : IMongoDatabaseSettings
     {
-        public string ConnectionString { get; set; } = "";
+        public string ConnectionString { get; set; } = "mongodb://root:Mongo%402024%23@localhost:27017/";
         public string DatabaseName { get; set; } = "UPBank";
         public string AddressCollectionName { get; set; } = "Address";
     }
