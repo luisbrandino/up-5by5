c057a4bd7dbbc110ec5411ea428d2ffc80eafb76
Cauã Rufino de Sá <CauaRufinodeSa@Gmail.com>
2024-06-22 18:10:05 -0300

    feat (Tests) implementation of Agencies Test.

---
+++ UPBank.Tests/Agencies/AgenciesTest.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UPBank.Agencies.Controllers;
+using UPBank.Agencies.Data;
+using UPBank.Models;
+
+namespace UPBank.Tests.Agencies
+{
+    public class AgenciesTest
+    {
+        private DbContextOptions<UPBankAgenciesContext> options;
+
+        public void InitializeDatabase()
+        {
+            options = new DbContextOptionsBuilder<UPBankAgenciesContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            using (var context = new UPBankAgenciesContext(options))
+            {
+                context.Agency.Add(new Agency
+                {
+                    Number = "123",
+                    Cnpj = "12345678901234",
+                    Restriction = false,
+                    AddressZipcode = "12345678"
+                });
+
+                context.Agency.Add(new Agency
+                {
+                    Number = "456",
+                    Cnpj = "12142678901434",
+                    Restriction = false,
+                    AddressZipcode = "12365678"
+                });
+
+                context.Agency.Add(new Agency
+                {
+                    Number = "789",
+                    Cnpj = "947295736125",
+                    Restriction = true,
+                    AddressZipcode = "06147678"
+                });
+                context.SaveChanges();
+            }
+        }
+
+        [Fact]
+        public void GetAgencies()
+        {
+            InitializeDatabase();
+
+            using (var context = new UPBankAgenciesContext(options))
+            {
+                var controller = new AgenciesController(context);
+                var result = controller.GetAgency().Result;
+
+                Assert.Equal(3, result.Value.Count());
+            }
+        }
+
+        [Fact]
+        public void GetAgency()
+        {
+            InitializeDatabase();
+
+            using (var context = new UPBankAgenciesContext(options))
+            {
+                var controller = new AgenciesController(context);
+                var result = controller.GetAgency("123").Result;
+
+                Assert.Equal("123", result.Value.Number);
+            }
+        }
+
+        [Fact]
+        public void PutAgency()
+        {
+            InitializeDatabase();
+
+            using (var context = new UPBankAgenciesContext(options))
+            {
+                var controller = new AgenciesController(context);
+                var result = controller.GetAgency("123").Result;
+
+                var agency = result.Value;
+                agency.Cnpj = "12345678901234";
+                agency.Restriction = true;
+
+                _ = controller.PutAgency("123", agency);
+
+                var resultUpdated = controller.GetAgency("123").Result;
+
+                Assert.True(resultUpdated.Value.Restriction);
+            }
+        }
+
+        [Fact]
+        public async void PostAgency()
+        {
+            InitializeDatabase();
+
+            using (var context = new UPBankAgenciesContext(options))
+            {
+                var controller = new AgenciesController(context);
+                var agency = new Agency
+                {
+                    Number = "101",
+                    Cnpj = "12345678901234",
+                    Restriction = false,
+                    AddressZipcode = "12345678"
+                };
+
+                await controller.PostAgency(agency);
+
+                var result = controller.GetAgency("101").Result;
+
+                Assert.Equal("101", result.Value.Number);
+            }
+        }
+
+        [Fact]
+        public void DeleteAgency()
+        {
+            InitializeDatabase();
+
+            using (var context = new UPBankAgenciesContext(options))
+            {
+                var controller = new AgenciesController(context);
+
+                _ = controller.DeleteAgency("123");
+
+                var result = controller.GetAgency("123").Result;
+
+                Assert.Null(result.Value);
+            }
+        }
+    }
+}
---
+++ UPBank.Tests/Agencies/ExternalApis.cs
@@ -0,0 +1,75 @@
+﻿using UPBank.Models;
+
+namespace UPBank.Tests.Agencies
+{
+    public class ExternalApis
+    {
+        private List<Employee> _employees;
+        private List<Address> _addresses;
+
+        public ExternalApis() 
+        {
+            _employees = new List<Employee>
+            {
+                new Employee
+                {
+                    Name = "Funcionário 1",
+                    Cpf = "12345678901",
+                    AgencyNumber = "123"
+                },
+                new Employee
+                {
+                    Name = "Funcionário 2",
+                    Cpf = "12345678902",
+                    AgencyNumber = "123"
+                },
+                new Employee
+                {
+                    Name = "Funcionário 3",
+                    Cpf = "12345678903",
+                    AgencyNumber = "456"
+                },
+                new Employee
+                {
+                    Name = "Funcionário 4",
+                    Cpf = "12345678904",
+                    AgencyNumber = "456"
+                }
+            };
+
+            _addresses = new List<Address>
+            {
+                new Address
+                {
+                    Zipcode = "12345678",
+                    Street = "Rua 1",
+                    Number = 123,
+                    Complement = "Casa",
+                    Neighborhood = "Bairro 1",
+                    City = "Cidade 1",
+                    State = "Estado 1"
+                },
+                new Address
+                {
+                    Zipcode = "12365678",
+                    Street = "Rua 2",
+                    Number = 456,
+                    Complement = "Casa",
+                    Neighborhood = "Bairro 2",
+                    City = "Cidade 2",
+                    State = "Estado 2"
+                }
+            };
+        }
+
+        public Address GetAddressFromExternalApiByZipcode(string zipcode)
+        {
+            return _addresses.Find(address => address.Zipcode == zipcode);
+        }
+
+        public List<Employee> GetEmployeeFromExternalApiByAgency(string agencyNumber)
+        {
+            return _employees.FindAll(employee => employee.AgencyNumber == agencyNumber);
+        }
+    }
+}
