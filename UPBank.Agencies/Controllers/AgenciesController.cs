77132ef439abf23787016e1b661c51205b5ada62
Cauã Rufino de Sá <CauaRufinodeSa@Gmail.com>
2024-06-22 20:20:44 -0300

    feat (Agencies Controllers) Implementation of CRUD methods and Gets for Restricted Accounts, Accounts By Profile and Accounts With Active Overdraft.

--- UPBank.Agencies/Controllers/AgenciesController.cs
+++ UPBank.Agencies/Controllers/AgenciesController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using UPBank.Agencies.Data;
+using UPBank.Agencies.Services;
+using UPBank.Enums;
 using UPBank.Models;
 
 namespace UPBank.Agencies.Controllers
@@ -15,32 +17,33 @@ namespace UPBank.Agencies.Controllers
     public class AgenciesController : ControllerBase
     {
         private readonly UPBankAgenciesContext _context;
+        private readonly AgencyService _agencyService;
 
-        public AgenciesController(UPBankAgenciesContext context) => _context = context;
+        public AgenciesController(UPBankAgenciesContext context)
+        {
+            _context = context;
+            _agencyService = new AgencyService();
+        }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Agency>>> GetAgency()
         {
-          if (_context.Agency == null)
-          {
-              return NotFound();
-          }
+            if (_context.Agency == null)
+                return NotFound();
+
             return await _context.Agency.ToListAsync();
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Agency>> GetAgency(string id)
         {
-          if (_context.Agency == null)
-          {
-              return NotFound();
-          }
+            if (_context.Agency == null)
+                return NotFound();
+
             var agency = await _context.Agency.FindAsync(id);
 
             if (agency == null)
-            {
                 return NotFound();
-            }
 
             return agency;
         }
@@ -49,9 +52,10 @@ namespace UPBank.Agencies.Controllers
         public async Task<IActionResult> PutAgency(string id, Agency agency)
         {
             if (id != agency.Number)
-            {
                 return BadRequest();
-            }
+
+            if (!Validations.CNPJ(agency.Cnpj))
+                return Problem("CNPJ is invalid.");
 
             _context.Entry(agency).State = EntityState.Modified;
 
@@ -62,13 +66,10 @@ namespace UPBank.Agencies.Controllers
             catch (DbUpdateConcurrencyException)
             {
                 if (!AgencyExists(id))
-                {
                     return NotFound();
-                }
+
                 else
-                {
                     throw;
-                }
             }
 
             return NoContent();
@@ -77,11 +78,14 @@ namespace UPBank.Agencies.Controllers
         [HttpPost]
         public async Task<ActionResult<Agency>> PostAgency(Agency agency)
         {
-          if (_context.Agency == null)
-          {
-              return Problem("Entity set 'UPBankAgenciesContext.Agency'  is null.");
-          }
+            if (_context.Agency == null)
+                return Problem("Entity set 'UPBankAgenciesContext.Agency'  is null.");
+
+            if (!Validations.CNPJ(agency.Cnpj))
+                return Problem("CNPJ is invalid.");
+
             _context.Agency.Add(agency);
+
             try
             {
                 await _context.SaveChangesAsync();
@@ -89,13 +93,10 @@ namespace UPBank.Agencies.Controllers
             catch (DbUpdateException)
             {
                 if (AgencyExists(agency.Number))
-                {
                     return Conflict();
-                }
+
                 else
-                {
                     throw;
-                }
             }
 
             return CreatedAtAction("GetAgency", new { id = agency.Number }, agency);
@@ -105,14 +106,12 @@ namespace UPBank.Agencies.Controllers
         public async Task<IActionResult> DeleteAgency(string id)
         {
             if (_context.Agency == null)
-            {
                 return NotFound();
-            }
+
             var agency = await _context.Agency.FindAsync(id);
+
             if (agency == null)
-            {
                 return NotFound();
-            }
 
             _context.Agency.Remove(agency);
             await _context.SaveChangesAsync();
@@ -120,9 +119,39 @@ namespace UPBank.Agencies.Controllers
             return NoContent();
         }
 
-        private bool AgencyExists(string id)
+        [HttpGet("Restricteds{agencyNumber}")]
+        public async Task<ActionResult<IEnumerable<Account>>> GetRestrictedAccounts(string agencyNumber)
         {
-            return (_context.Agency?.Any(e => e.Number == id)).GetValueOrDefault();
+            var accounts = await _agencyService.GetAccountsFromAgency(agencyNumber);
+
+            if (!accounts.Any())
+                return Problem("There are no accounts restricted!");
+
+            return accounts.Where(a => a.Restriction).ToList();
         }
+
+        [HttpGet("ByProfile{profile}")]
+        public async Task<ActionResult<IEnumerable<Account>>> GetAccountsByProfile(EProfile profile)
+        {
+            var accounts = await _agencyService.GetAccountsByProfile(profile);
+
+            if (!accounts.Any())
+                return Problem("There are no accounts that match this profile!");
+
+            return (ActionResult)accounts;
+        }
+
+        [HttpGet("WithActiveOverdraft")]
+        public async Task<ActionResult<IEnumerable<Account>>> GetAccountsWithActiveOverdraft()
+        {
+            var accounts = await _agencyService.GetAccountsWithActiveOverdraft();
+
+            if (!accounts.Any())
+                return Problem("There are no accounts with active overdraft!");
+
+            return (ActionResult)accounts;
+        }
+
+        private bool AgencyExists(string id) => (_context.Agency?.Any(e => e.Number == id)).GetValueOrDefault();
     }
 }
