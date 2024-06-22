792327af26243de3e0ad88a890489e312ecd819f
Cauã Rufino de Sá <CauaRufinodeSa@Gmail.com>
2024-06-22 20:22:34 -0300

    feat (Agencies Services) Implementation of requests(Restricteds, By Profile and With Active Overdraft) for AccountAPI.

---
+++ UPBank.Agencies/Services/AgencyService.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using UPBank.Enums;
+using UPBank.Models;
+
+namespace UPBank.Agencies.Services
+{
+    public class AgencyService
+    {
+        private readonly string _Account = "https://localhost:####/api/Accounts/";
+    
+        public async Task<IEnumerable<Account>> GetAccountsFromAgency(string agencyNumber)
+        {
+            var url = _Account + agencyNumber;
+            List<Account> accounts = new List<Account>();
+
+            try
+            {
+                using var client = new HttpClient();
+                var response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+
+                    if (!string.IsNullOrEmpty(json))
+                        accounts = JsonConvert.DeserializeObject<IEnumerable<Account>>(json).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return accounts;
+        }
+
+        public async Task<IEnumerable<Account>> GetAccountsByProfile(EProfile profile)
+        {
+            var url = _Account + "####" + profile;
+            List<Account> accounts = new List<Account>();
+
+            try
+            {
+                using var client = new HttpClient();
+                var response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+
+                    if (!string.IsNullOrEmpty(json))
+                        accounts = JsonConvert.DeserializeObject<IEnumerable<Account>>(json).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return accounts;
+        }
+
+        public async Task<IEnumerable<Account>> GetAccountsWithActiveOverdraft()
+        {
+            var url = _Account + "WithActiveOverdraft";
+            List<Account> accounts = new List<Account>();
+
+            try
+            {
+                using var client = new HttpClient();
+                var response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+
+                    if (!string.IsNullOrEmpty(json))
+                        accounts = JsonConvert.DeserializeObject<IEnumerable<Account>>(json).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ UPBank.Agencies/Services/Validations.cs
@@ -0,0 +1,32 @@
+﻿namespace UPBank.Agencies.Services
+{
+    public static class Validations
+    {
+        public static bool CNPJ(string cnpj)
+        {
+            if (cnpj.Length != 14)
+                return false;
+
+            if (cnpj.Count(value => value == cnpj[0]) > cnpj.Length - 1)
+                return false;
+
+            return ValidarDV(cnpj, 1) && ValidarDV(cnpj, 2);
+        }
+
+        private static bool ValidarDV(string cpf, int dv)
+        {
+            int result = 0;
+
+            for (int posicao = 0, multiplicador = 4 + dv; posicao < 11 + dv; posicao++, multiplicador--)
+            {
+                if (multiplicador == 1)
+                    multiplicador = 9;
+                result += int.Parse(cpf.Substring(posicao, 1)) * multiplicador;
+            }
+
+            result %= 11;
+
+            return (result == 0 || result == 1 ? 0 : 11 - result) == int.Parse(cpf.Substring(11 + dv, 1));
+        }
+    }
+}
