0b1ac65e87a24e6219da550e5d4543126aa7bfad
Cauã Rufino de Sá <CauaRufinodeSa@Gmail.com>
2024-06-25 14:13:06 -0300

    feat (Data) creation of DeletedAgency context and override in SaveChangesAsync to save agency in deleted table before deleting as well as retrieving from deleted table when http post same agency cnpj.

--- UPBank.Agencies/Data/UPBankAgenciesContext.cs
+++ UPBank.Agencies/Data/UPBankAgenciesContext.cs
@@ -1,19 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using UPBank.Models;
 
 namespace UPBank.Agencies.Data
 {
     public class UPBankAgenciesContext : DbContext
     {
-        public UPBankAgenciesContext (DbContextOptions<UPBankAgenciesContext> options)
+        public UPBankAgenciesContext(DbContextOptions<UPBankAgenciesContext> options)
             : base(options)
         {
         }
 
         public DbSet<UPBank.Models.Agency> Agency { get; set; } = default!;
+        public DbSet<UPBank.Models.DeletedAgency> DeletedAgency { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Agency>()
+                        .ToTable("Agency");
+
+            modelBuilder.Entity<DeletedAgency>()
+                        .ToTable("DeletedAgency");
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<Agency>())
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    var agency = entry.Entity;
+
+                    var deletedAgency = Set<DeletedAgency>().FirstOrDefault(deleted => deleted.Cnpj == agency.Cnpj);
+
+                    if (deletedAgency != null)
+                        Set<DeletedAgency>().Remove(deletedAgency);
+                }
+                
+                else if (entry.State == EntityState.Deleted)
+                {
+                    var deletedAgency = new DeletedAgency
+                    {
+                        Number = entry.OriginalValues.GetValue<string>("Number"),
+                        Cnpj = entry.OriginalValues.GetValue<string>("Cnpj"),
+                        Restriction = entry.OriginalValues.GetValue<bool>("Restriction"),
+                        Employees = entry.OriginalValues.GetValue<List<Employee>>("Employees"),
+                        Address = entry.OriginalValues.GetValue<Address>("Address"),
+                        AddressZipcode = entry.OriginalValues.GetValue<string>("AddressZipcode"),
+                    };
+
+                    Set<DeletedAgency>().Add(deletedAgency);
+                }
+            }
+
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
     }
 }
