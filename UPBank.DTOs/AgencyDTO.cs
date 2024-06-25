11f4225ee4e9208a5663c0b5388eeae6b779e242
Cauã Rufino de Sá <CauaRufinodeSa@Gmail.com>
2024-06-25 14:09:47 -0300

    feat (DTOs) AgencyDTO to use in Post http request.

---
+++ UPBank.DTOs/AgencyDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Net;
+using System.Text.Json.Serialization;
+using UPBank.Models;
+
+namespace UPBank.DTOs
+{
+    public class AgencyDTO
+    {
+        public string Cnpj { get; set; }
+        public Employee Manager { get; set; }
+        public AddressDTO Address { get; set; }
+    }
+}
