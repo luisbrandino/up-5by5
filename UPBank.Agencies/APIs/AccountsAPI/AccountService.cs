5d4868f1b596a644af6eeaa0872a6bbbf210fe82
luisbrandino <luisbrandino.contato@gmail.com>
2024-06-26 18:35:36 -0300

    feat: fixed url

--- UPBank.Agencies/APIs/AccountsAPI/AccountService.cs
+++ UPBank.Agencies/APIs/AccountsAPI/AccountService.cs
@@ -13,7 +13,7 @@ namespace UPBank.Agencies.APIs.AccountsAPI
         public async Task<IEnumerable<Account>> GetRestrictedAccounts(string agencyNumber)
         {
             using var client = new HttpClient();
-            var response = await client.GetAsync(_Account + agencyNumber + "/restricted");
+            var response = await client.GetAsync(_Account + "agency/" + agencyNumber + "/restricted");
 
             return await ApiUtils<IEnumerable<Account>>.GetObjectFromResponse(response) ?? new List<Account>();
         }
@@ -21,7 +21,7 @@ namespace UPBank.Agencies.APIs.AccountsAPI
         public async Task<IEnumerable<Account>> GetAccountsByProfile(string agencyNumber, EProfile profile)
         {
             using var client = new HttpClient();
-            var response = await client.GetAsync(_Account + agencyNumber + "/profile/" + profile);
+            var response = await client.GetAsync(_Account + "agency/" + agencyNumber + "/profile/" + profile);
 
             return await ApiUtils<IEnumerable<Account>>.GetObjectFromResponse(response) ?? new List<Account>();
         }
@@ -29,7 +29,7 @@ namespace UPBank.Agencies.APIs.AccountsAPI
         public async Task<IEnumerable<Account>> GetAccountsWithActiveOverdraft(string agencyNumber)
         {
             using var client = new HttpClient();
-            var response = await client.GetAsync(_Account + agencyNumber + "/activeloans");
+            var response = await client.GetAsync(_Account + "agency/" + agencyNumber + "/activeloans");
 
             return await ApiUtils<IEnumerable<Account>>.GetObjectFromResponse(response) ?? new List<Account>();
         }
